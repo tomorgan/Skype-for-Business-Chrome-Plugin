da6deb3c4aaa8b15da5edbc052ec05a3698e93cd
Tom Morgan <tom.r.morgan@gmail.com>
2016-05-11 22:44:07 +0100

    The big initial check-in for v1 ;)

---
+++ chrome_sfb/makecall.js
@@ -0,0 +1,25 @@
+// Set up context menu at install time.
+chrome.runtime.onInstalled.addListener(function() {
+  var context = "selection";
+  var title = "Call in Skype for Business";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                         "id": "context" + context});  
+});
+
+// add click event
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+    
+  chrome.tabs.executeScript ({
+	   code: 'if (document.contains(document.getElementById("ThoughtStuffSfBpluginlink"))) {            document.getElementById("ThoughtStuffSfBpluginlink").remove();}'});
+  
+chrome.tabs.executeScript({ 			
+		 code: 'var a = document.createElement("a");var linkText = document.createTextNode("AAAA");a.appendChild(linkText);a.title = "my title text";a.href ="tel://' + info.selectionText + '";a.id = "ThoughtStuffSfBpluginlink";document.body.appendChild(a);  document.getElementById("ThoughtStuffSfBpluginlink").click();'}); 
+		 
+		  chrome.tabs.executeScript ({
+	   code: 'if (document.contains(document.getElementById("ThoughtStuffSfBpluginlink"))) {            document.getElementById("ThoughtStuffSfBpluginlink").remove();}'});
+		
+
+};
